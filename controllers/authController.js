cbc76b38d33e7dfd2e26b2ee456d6dfd132a0e2f
DarckSould <darteaga702@gmail.com>
2025-06-23 00:00:20 -0500

    desplegado

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,7 +18,7 @@ const login = async (req, res) => {
     res
       .cookie('token', token, {
         httpOnly: true,
-        secure: false, // Usa true en producción con HTTPS
+        secure: true, // Usa true en producción con HTTPS
         sameSite: 'lax',
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 días
       })
